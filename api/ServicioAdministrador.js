cd479417d676020b687062f99267b4722d3d6846
Laura Edith López Fregoso <laura.lopez204820@potros.itson.edu.mx>
2022-03-21 18:05:25 -0700

    Se cubre el CRUD completo con respuestas dummy
    Co-authored-by: cr-ls <carlos.huerta013@gmail.com>

--- api/ServicioAdministrador.js
+++ api/ServicioAdministrador.js
@@ -2,24 +2,31 @@ const mongoose = require('mongoose');
 const control = require('../control/control');;
 
 //Crear un administrador 
-exports.insertarAdministrador =  async (req, res) =>{
- 
+exports.insert =  async (req, res) =>{
   try {
-    control.administrador.insertar(
-        req.body.userId,
-        req.body.nombre,
-        req.body.contrasenia,
-        req.body.fechaRegistro,
-        req.body.email,
-        req.body.nombreAdministrador,
-        req.body.area,
-        req.body.puesto,
-        req.body.telefono
-    )
+    // Interacción con el acceso a datos
+    // control.administrador.insertar(
+    //     req.body.userId,
+    //     req.body.nombre,
+    //     req.body.contrasenia,
+    //     req.body.fechaRegistro,
+    //     req.body.email,
+    //     req.body.nombreAdministrador,
+    //     req.body.area,
+    //     req.body.puesto,
+    //     req.body.telefono
+    // )
+    // res.status(201).json({
+    //   status: 'Se ha almacenado en la base de datos el administrador',
+    //   data: {
+    //     administrador: req.body
+    //   }
+    // });
+    
     res.status(201).json({
       status: 'sucess',
       data: {
-        user: req.body
+        administrador: req.body
       }
     });
 
@@ -31,68 +38,124 @@ exports.insertarAdministrador =  async (req, res) =>{
   }
 };
 
-// //Obtener la lista de usuarios 
-// exports.getAllUsers = async (req, res) =>{
-//   try{
-//      const usuarios = await userModel.find();
-//      res.send(usuarios);
-//   }catch(err){
-//     res.send(err);
-//   }
-// }
+//Obtener la lista de administradores 
+exports.getAll = async (req, res) =>{
+  try{
+    // Interacción con el acceso a datos
+    // control.administrador.obtenerTodos().then(admin => {
+    //   res.send(admin)
+    // })
 
-// //Obtener usuario 
-// exports.getUser = catchAsync(async (req, res, next) =>{
-  
+    // Respuesta dummy
+    dummy = [{
+        "userId": "390",
+        "nombre": "Angel de jesus",
+        "contrasenia": "angel1232!",
+        "fechaRegistro": "25/11/2019",
+        "email": "angel_jesus@hotmail.com",
+        "nombreAdministrador": "Representante",
+        "area": "Administrativa",
+        "puesto": "Gerencial",
+        "telefono": "6444105210",
+      },
+      {
+        "userId": "100",
+        "nombre": "Veronica Reyes",
+        "contrasenia": "veronica2413!",
+        "fechaRegistro": "25/12/2021",
+        "email": "verovero@gmail.com",
+        "nombreAdministrador": "Local",
+        "area": "Administrativa",
+        "puesto": "Local",
+        "telefono": "6871596633",
+      }
+    ]
+    res.send(dummy);
+  }catch(err){
+    res.send(err);
+  }
+}
+
+//Obtener administrador 
+exports.get = async (req, res, next) =>{
+  try{
+    // Interacción con el acceso a datos
+    // control.administrador.obtener(req.params.userId).then(admin => {
+    //   res.send(admin[0])
+    // })
+
+    //Respuesta dummy
+    dummy = {
+      "userId": req.params.userId,
+      "nombre": "Angel de jesus",
+      "contrasenia": "angel1232!",
+      "fechaRegistro": "25/11/2019",
+      "email": "angel_jesus@hotmail.com",
+      "nombreAdministrador": "Representante",
+      "area": "Administrativa",
+      "puesto": "Gerencial",
+      "telefono": "6444105210",
+  }
+    res.send(dummy)
+  }catch(err){
+    res.send(err);
+  }
+}
+
+//Update Administrador
+exports.update = async (req, res, next) => {
+  try {
+    // Interacción con el acceso a datos
+    // control.administrador.actualizar(
+    //   req.body.userId,
+    //   req.body.nombre,
+    //   req.body.contrasenia,
+    //   req.body.fechaRegistro,
+    //   req.body.email,
+    //   req.body.nombreAdministrador,
+    //   req.body.area,
+    //   req.body.puesto,
+    //   req.body.telefono)
+
+    // control.administrador.obtener(req.params.userId).then(admin => {
+    //   res.status(200).json({
+    //   status: 'success',
+    //   administrador: admin[0]
+    //   }); 
+    // })
+
+    // Respuesta dummy
+    dummy = {
+      "userId": req.params.userId,
+      "nombre": "Angel de jesus",
+      "contrasenia": "angel1232!",
+      "fechaRegistro": "25/11/2019",
+      "email": "angel_jesus@hotmail.com",
+      "nombreAdministrador": "Representante",
+      "area": "Administrativa",
+      "puesto": "Gerencial",
+      "telefono": "6444105210",
+    }
+
+    res.status(200).json({
+      status: 'success',
+      administrador: dummy
+    }); 
+  } catch (error) {
+      res.send(error);
+  }
+};
+
+//Delete Administrador 
+exports.delete = async (req, res) =>{
+
+  try {
+    res.status(204).json({
+      status: 'success',
+      adminisstrador: null
+    });
+  } catch (error) {
+      res.send(error);
+  }
     
-//   const user = await userModel.findById(req.params.id);
-
-//   if(!user){
-//     return next(new AppError('Not user found with the ID', 404));
-//   }
-  
-//   res.status(200).json({
-//     status: 'sucess',
-//     data:{
-//       user
-//     }
-//   });
-// });
-
-// //Update User 
-// exports.updateUser = catchAsync(async (req, res, next) => {
-//   const user = await userModel.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true,
-//     runValidators: true
-//   });
-
-//   if (!user) {
-//     return next(new AppError('No tour found with that ID', 404));
-//   }
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       user
-//     }
-//   });
-// });
-
-
-// //Delete User 
-// exports.deleteUser = catchAsync(async (req, res, next) =>{
-//     const user = await userModel.findByIdAndDelete(req.params.id);
-
-//     if(!user){
-//       return next(new AppError('No tour found with that ID', 404));
-//     }
-
-//     res.status(204).json({
-//       status: 'success',
-//       data: null
-//     });
-// })
-
-
-
-
+}
--- api/server.js
+++ api/server.js
@@ -1,16 +1,23 @@
 var express = require('express');
-const { insertarAdministrador } = require('./ServicioAdministrador');
+const ServicioAdministrador = require('./ServicioAdministrador');
 require('dotenv').config();
 
 var app = express();
-
 
 app.use(express.json());
 
 const router = express.Router();
 
-//Recibir el usuario
-router.route('/').post(insertarAdministrador);
+//Crear administrador
+router.route('/').post(ServicioAdministrador.insert);
+// Obtener todos los administradores
+router.route('/').get(ServicioAdministrador.getAll);
+// Obtener un administrador
+router.route('/:userId').get(ServicioAdministrador.get);
+// Borrar un administrador
+router.route('/:userId').delete(ServicioAdministrador.delete);
+// Actualizar un adminiostrador
+router.route('/:userId').put(ServicioAdministrador.update);
 
 //Ruta especifica
 app.use('/api/administrador', router);
